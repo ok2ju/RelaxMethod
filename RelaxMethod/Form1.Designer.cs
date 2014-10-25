c9d7cff6e2f07189da1c3b856a7e24784742aa46
ok2ju <soulSpringg@gmail.com>
2014-10-25 12:41:05 +0300

    repo init

---
+++ RelaxMethod/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace RelaxMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxLeft = new System.Windows.Forms.TextBox();
+            this.textBoxDown = new System.Windows.Forms.TextBox();
+            this.textBoxUp = new System.Windows.Forms.TextBox();
+            this.textBoxRight = new System.Windows.Forms.TextBox();
+            this.textBoxitercount = new System.Windows.Forms.TextBox();
+            this.textBoxerror = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonInit = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(342, 226);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxLeft
+            // 
+            this.textBoxLeft.Location = new System.Drawing.Point(15, 133);
+            this.textBoxLeft.Name = "textBoxLeft";
+            this.textBoxLeft.Size = new System.Drawing.Size(36, 20);
+            this.textBoxLeft.TabIndex = 1;
+            // 
+            // textBoxDown
+            // 
+            this.textBoxDown.Location = new System.Drawing.Point(207, 263);
+            this.textBoxDown.Name = "textBoxDown";
+            this.textBoxDown.Size = new System.Drawing.Size(36, 20);
+            this.textBoxDown.TabIndex = 2;
+            // 
+            // textBoxUp
+            // 
+            this.textBoxUp.Location = new System.Drawing.Point(207, 5);
+            this.textBoxUp.Name = "textBoxUp";
+            this.textBoxUp.Size = new System.Drawing.Size(36, 20);
+            this.textBoxUp.TabIndex = 3;
+            // 
+            // textBoxRight
+            // 
+            this.textBoxRight.Location = new System.Drawing.Point(405, 133);
+            this.textBoxRight.Name = "textBoxRight";
+            this.textBoxRight.Size = new System.Drawing.Size(36, 20);
+            this.textBoxRight.TabIndex = 4;
+            // 
+            // textBoxitercount
+            // 
+            this.textBoxitercount.Location = new System.Drawing.Point(540, 12);
+            this.textBoxitercount.Name = "textBoxitercount";
+            this.textBoxitercount.Size = new System.Drawing.Size(103, 20);
+            this.textBoxitercount.TabIndex = 5;
+            this.textBoxitercount.TextChanged += new System.EventHandler(this.textBoxitercount_TextChanged);
+            // 
+            // textBoxerror
+            // 
+            this.textBoxerror.Location = new System.Drawing.Point(540, 38);
+            this.textBoxerror.Name = "textBoxerror";
+            this.textBoxerror.Size = new System.Drawing.Size(103, 20);
+            this.textBoxerror.TabIndex = 6;
+            this.textBoxerror.TextChanged += new System.EventHandler(this.textBoxerror_TextChanged);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(540, 64);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(103, 20);
+            this.textBoxN.TabIndex = 7;
+            this.textBoxN.TextChanged += new System.EventHandler(this.textBoxN_TextChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(446, 211);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(90, 46);
+            this.buttonStart.TabIndex = 8;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonInit
+            // 
+            this.buttonInit.Location = new System.Drawing.Point(506, 150);
+            this.buttonInit.Name = "buttonInit";
+            this.buttonInit.Size = new System.Drawing.Size(90, 46);
+            this.buttonInit.TabIndex = 9;
+            this.buttonInit.Text = "Init";
+            this.buttonInit.UseVisualStyleBackColor = true;
+            this.buttonInit.Click += new System.EventHandler(this.buttonInit_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(553, 211);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(90, 46);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(481, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Iter Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(503, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Error";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(517, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "N";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(540, 90);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(103, 20);
+            this.textBoxTime.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Time";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(344, 263);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "View";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 292);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonInit);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxerror);
+            this.Controls.Add(this.textBoxitercount);
+            this.Controls.Add(this.textBoxRight);
+            this.Controls.Add(this.textBoxUp);
+            this.Controls.Add(this.textBoxDown);
+            this.Controls.Add(this.textBoxLeft);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxLeft;
+        private System.Windows.Forms.TextBox textBoxDown;
+        private System.Windows.Forms.TextBox textBoxUp;
+        private System.Windows.Forms.TextBox textBoxRight;
+        private System.Windows.Forms.TextBox textBoxitercount;
+        private System.Windows.Forms.TextBox textBoxerror;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonInit;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ RelaxMethod/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelaxMethod
+{
+    public partial class Form1 : Form
+    {
+        int IterCount;
+        DateTime startTime, endTime;
+        Bitmap bm;
+        Graphics gr;
+        int Lx, Ly;
+        double[,] T, TT;
+        int N;
+        double Tleft, TRight, TUp, TDown;
+        Color Col;
+        double kx, Err1;
+        int dx;
+        SolidBrush brush1;
+        Boolean priznak;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Lx = pictureBox1.Height;
+            Ly = pictureBox1.Height;
+
+            bm = new Bitmap(Lx + 1, Ly + 1);
+        }
+
+        private void showpic()
+        {
+            for (int j = 0; j <= N; j++)
+            {
+                for (int i = 0; i <= N; i++)
+                {
+                    if (T[i, j] >= 0)
+                    {
+                        Col = Color.FromArgb(Convert.ToInt32(255 * (T[i, j]) / 13), 0, 0);
+                    }
+                    else
+                    {
+                        Col = Color.FromArgb(0, 0, Convert.ToInt32(-255 * T[i, j] / 13));
+                    }
+                    brush1.Color = Col;
+                    gr.FillRectangle(brush1, j * dx, i * dx, dx, dx);
+                }
+            }
+            pictureBox1.Image = bm;
+        }
+
+        //Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+            priznak = false;
+            IterCount = 0;
+            if (checkBox1.Checked == false)
+            {
+                do
+                {
+                    priznak = true;
+                    IterCount += 1;
+
+                    for (int j = 1; j <= N - 1; j++)
+                    {
+                        for (int i = 1; i <= N - 1; i++)
+                        {
+                            //релаксация
+                            TT[i, j] = 0.25 * (T[i - 1, j] + T[i + 1, j] + T[i, j - 1] + T[i, j + 1]);
+                            if (Math.Abs(TT[i, j] - T[i, j]) > Err1) { priznak = false; }
+                        }
+                    }
+
+                    for (int j = 1; j <= N - 1; j++)
+                    {
+                        for (int i = 1; i <= N - 1; i++)
+                        {
+                            T[i, j] = TT[i, j];
+                        }
+                    }
+                } while (priznak == false || IterCount < 10000);
+
+                if (priznak == false) textBoxerror.BackColor = Color.Red;
+                else { textBoxerror.BackColor = Color.White; }
+            }
+            else { timer1.Enabled = true; }
+
+            endTime = DateTime.Now;
+            showpic();
+            textBoxTime.Text = Convert.ToString(endTime.Minute * 60 + endTime.Second - startTime.Minute * 60 - startTime.Second);
+        }
+
+        private void buttonInit_Click(object sender, EventArgs e)
+        {
+            textBoxitercount.Text = "";
+            Err1 = Convert.ToDouble(textBoxerror.Text);
+            N = Convert.ToInt32(textBoxN.Text);
+            Tleft = Convert.ToDouble(textBoxLeft.Text);
+            TRight = Convert.ToDouble(textBoxRight.Text);
+            TDown = Convert.ToDouble(textBoxDown.Text);
+            TUp = Convert.ToDouble(textBoxUp.Text);
+            T = new double[N + 1, N + 1];
+            TT = new double[N + 1, N + 1];
+
+            kx = Lx;
+            dx = Convert.ToInt32((kx / (N + 1)));
+            pictureBox1.Width = (pictureBox1.Height / dx) * dx;
+            pictureBox1.Height = pictureBox1.Width;
+            Lx = pictureBox1.DisplayRectangle.Width;
+            Ly = pictureBox1.DisplayRectangle.Width;
+
+            brush1 = new SolidBrush(Color.Red);
+            gr = Graphics.FromImage(bm);
+            pictureBox1.Image = bm;
+            IterCount = 0;
+
+            //Граничные условия
+            for (int i = 0; i <= N; i++)
+            {
+                T[0, i] = TUp;
+                T[i, 0] = Tleft;
+                T[N, i] = TDown;
+                T[i, N] = TRight;
+            }
+            //начальные условия
+            for (int j = 1; j <= N - 1; j++)
+            {
+                for (int i = 1; i <= N - 1; i++)
+                {
+                    T[i, j] = 8;
+                }
+            }
+            showpic();
+        }//инициализация
+
+        private void textBoxitercount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxerror_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Stop
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            if (priznak == false) textBoxerror.BackColor = Color.Red;
+            else { textBoxerror.BackColor = Color.White; }
+        }
+
+        //timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            priznak = true;
+            IterCount += 1;
+
+            for (int j = 1; j <= N - 1; j++)
+            {
+                for (int i = 1; i <= N - 1; i++)
+                {
+                    //релаксация
+                    TT[i, j] = 0.25 * (T[i - 1, j] + T[i + 1, j] + T[i, j - 1] + T[i, j + 1]);
+                    if (Math.Abs(TT[i, j] - T[i, j]) > Err1) { priznak = false; }
+                }
+            }
+            showpic();
+            textBoxitercount.Text = Convert.ToString(IterCount);
+            if (priznak == false) textBoxerror.BackColor = Color.Red;
+            else { textBoxerror.BackColor = Color.White; }
+
+            for (int j = 1; j <= N - 1; j++)
+            {
+                for (int i = 1; i <= N - 1; i++)
+                {
+                    T[i, j] = TT[i, j];
+                }
+            }
+        }
+
+        private void textBoxN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
